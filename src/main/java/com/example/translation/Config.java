b01cf03423546352b7e171db86180e168b7901ce
Sam Stevens <samdjstevens@googlemail.com>
2021-02-12 14:14:25 +0000

    use DefaultCredentialsProvider so that the container can use assume the task role

--- src/main/java/com/example/translation/Config.java
+++ src/main/java/com/example/translation/Config.java
@@ -2,10 +2,8 @@ package com.example.translation;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsCredentialsProviderChain;
-import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.SystemPropertyCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.services.translate.TranslateClient;
 
 @Configuration
@@ -18,11 +16,7 @@ public class Config {
             .build();
     }
 
-    private AwsCredentialsProviderChain credentialsProviderChain () {
-        return AwsCredentialsProviderChain.of(
-            SystemPropertyCredentialsProvider.create(),
-            EnvironmentVariableCredentialsProvider.create(),
-            ProfileCredentialsProvider.create()
-        );
+    private AwsCredentialsProvider credentialsProviderChain () {
+        return DefaultCredentialsProvider.create();
     }
 }
